1d3519bee91e3ce1e91f87f231bb70d8b4724e0c
DrEPL <dolnickenzanza@gmail.com>
2024-05-16 11:37:32 +0100

    add node_modules

--- safe_auto_ecole/urls.py
+++ safe_auto_ecole/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index') 
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
